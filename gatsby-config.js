78d2a0ae92906b75184ea676fcf42b37ea80022b
katrlv <51521545+katrlv@users.noreply.github.com>
2020-02-28 13:14:14 -0500

    remove logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Katryn's Maker Lab II Portfolio`,
     author: {
       name: "Katryn's Maker Lab II Portfolio",
-      image: "https://media1.giphy.com/media/fo2dhRTmaULbStoFkX/giphy.gif",
+      image: "",
       biography: "Katryn Relleve is a User Experience Design student currently in her second year at Wilfrid Laurier University. \
           This is her portfolio showcasing Maker Lab II projects.",
     },
