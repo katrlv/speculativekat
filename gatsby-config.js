d8336c966b3ff163f0a6ff8bcfa0d0b3408df06e
katrlv <51521545+katrlv@users.noreply.github.com>
2020-04-17 09:01:49 -0400

    update bio

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,9 +14,9 @@ module.exports = {
       "mailto:rell1960@mylaurier.ca",
     ],
     about: '<p>Katryn Relleve is a User Experience Design student currently in her second year at Wilfrid Laurier University. \
-    Previously, she worked as a Junior Web Designer for WCDSB. \
+    Previously, she worked as a Junior Web Designer for WCDSB. She is currently the Web Director for The Sputnik (WLUSP). \
     Katryn is most interested in learning how to create designs driven by people and purpose. \
-    This is her portfolio showcasing Maker Lab projects, which aim at achieving social good with the use of critical making. \
+    This is her portfolio showcasing Maker Lab projects, which aim at achieving social good with the use of speculative design strategies and critical making. \
     </p> \
     ',
   },
